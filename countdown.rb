a26f0f39c9fffa4e4ae1f0eae99010bdf3a6b79c
Felix <barosiofelix@gmail.com>
2022-10-22 17:13:03 +0300

    Completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown value
+    
+    while value >= 1
+        puts "#{value} SECOND(S)!"
+        value -= 1
+    end
+
+    'HAPPY NEW YEAR!'
+
+end
+
+
+def countdown_with_sleep value
+
+    while value >= 1
+        puts "#{value} SECOND(S)!"
+        value -= 1
+        sleep 5
+    end
+
 end
